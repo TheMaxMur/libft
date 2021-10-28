c2837553994cb6769bce98973aeb07079a9632fb
TheMaxMur <muravjev.mak@yandex.ru>
2021-10-28 18:43:58 +0300

    edit script

--- runtest.sh
+++ runtest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-bash /home/maxmur/Documents/code/git/libft-unit-test/libft-war-machine/grademe.sh
-cd /home/maxmur/Documents/code/git/libft-unit-test/libft-unit-test/
+bash ./libft-war-machine/grademe.sh
+cd ./libft-unit-test/
 make fclean > /dev/null
 make > /dev/null
 ./run_test
